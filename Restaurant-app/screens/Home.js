fe9213ad65440a96453501e9feeb4516dd1b8ffd
Juanjo Ortiz <jjortiz@ilernaonline.com>
2022-02-08 12:01:34 +0100

    starting restaurant app

---
+++ Restaurant-app/screens/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+    return <Text style={styles.text}>Prueba restaurante </Text>;
+};
+
+export default HomeScreen;
